812f2ff3207dd5f93871f7a03fcf4afcc70e2bc9
004IMY744 <ranjiithsayanth@ibm.com>
2024-01-19 17:49:28 +0530

    used webclient

--- Employees/Employees/src/main/java/com/example/employees/Employees/repo/EmployeeRepoImpl.java
+++ Employees/Employees/src/main/java/com/example/employees/Employees/repo/EmployeeRepoImpl.java
@@ -5,6 +5,9 @@ import com.example.employees.Employees.ENTITY.Employeeentity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Repository
 
@@ -15,14 +18,27 @@ public class EmployeeRepoImpl  {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    WebClient webClient;
+
     public Employeeentity getEmployeeDetails(int id ){
         Employeeentity employeeentity = employeegetdetails.findById(id).get();
+        //AddressResponse addressResponse=restTemplate.getForObject("http://localhost:8080/address-app/api/getbyID/{id}", AddressResponse.class,id);
+        try {
+            AddressResponse addressResponse = webClient.get()
+                    .uri("http://localhost:8080/address-app/api/getbyID/{id}", id)
+                    .retrieve()
+                    .bodyToMono(AddressResponse.class)
+                    .block();
+            System.out.println(addressResponse);
+            employeeentity.setAddressResponse(addressResponse);
+        }
 
+        catch (WebClientResponseException.NotFound ex) {
+            System.out.println("404 Not Found: " + ex.getMessage());
+        }
 
-        AddressResponse addressResponse=restTemplate.getForObject("http://localhost:8080/address-app/api/getbyID/{id}", AddressResponse.class,id);
-        System.out.println(addressResponse);
-        employeeentity.setAddressResponse(addressResponse);
-         return employeeentity;
+        return employeeentity;
     }
 
 
